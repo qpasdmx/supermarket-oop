19ee15102a9d2e7c108a85b45e1b24a1c8cc74d2
OSNAT DAYAN <93711505+qpasdmx@users.noreply.github.com>
2022-02-11 12:48:52 +0200

    Update Consumer.ts

--- Consumer.ts
+++ Consumer.ts
@@ -1,7 +1,10 @@
 export class Consumer implements IPerson {
+    firstName:string;
+    lastName:string;
     isClubMember: boolean;
-    constructor(name: string, age: number,  isClubMember: boolean) {
-        super(name, age);
+    constructor(firstName:string, lastName:string,  isClubMember: boolean) {
+        this.firstName= firstName;
+        this.lastName=lastName;
         this.isClubMember =  isClubMember;
 
     }

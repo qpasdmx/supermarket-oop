d277d4100e017cb2538fc9090c0a5cb08b0fccfe
OSNAT DAYAN <93711505+qpasdmx@users.noreply.github.com>
2022-02-11 12:12:10 +0200

    Create Product.ts

---
+++ Product.ts
@@ -0,0 +1,12 @@
+class Product {
+    price: number;
+    description: string;
+    company: string;
+    code: string;
+    constructor(price: number, description: string, company: string, code: string) {
+        this.price = price;
+        this.description = description;
+        this.company = company;
+        this.code = code;
+    }
+}

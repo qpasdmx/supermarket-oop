c4fd8779383108d6863484e18ec8cb5887026d10
OSNAT DAYAN <93711505+qpasdmx@users.noreply.github.com>
2022-02-11 12:38:21 +0200

    Update StaffMember.ts

--- StaffMember.ts
+++ StaffMember.ts
@@ -1,4 +1,4 @@
-export class StaffMember extends IPerson {
+export class StaffMember implements IPerson {
     role: string;
     constructor(name: string, age: number, role: string) {
         super(name, age);
